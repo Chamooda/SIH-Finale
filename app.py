cddc964d4cae246adc5a73e862eeec0fe73e2f9b
Asad Hadi <78380751+Chamooda@users.noreply.github.com>
2023-09-23 07:24:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ app = Flask(__name__)
 @app.route('/urls/<string:urls>')
 def urls_playboy(urls):
     urls = urls.replace(' ','/')
-    output = Mainly(urls)
     return jsonify({
         'status': 'ok',
         'message': 'Hello World!',
-        'urls': output})
+        'urls': urls})
     
 
 @app.route('/justcheckin')
